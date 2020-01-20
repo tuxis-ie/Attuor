36c702e18779764b43c766f63e3f596763564000
Mark Schouten <mark@tuxis.nl>
2020-01-20 22:35:45 +0100

    First go...

---
+++ attuor-notify/attuor/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# pylint: disable=invalid-name, wrong-import-position
+
+"""
+Attuor, observing your services
+Copyright (C) 2020  Mark Schouten, Tuxis B.V. <mark@tuxis.nl>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
